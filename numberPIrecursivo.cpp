68734c1c3164aaa5314a52075bbd185697d931a0
Cristian Mundo <37192145+AzzarCM@users.noreply.github.com>
2019-09-11 19:46:09 -0600

    Update numberPIrecursivo.cpp

--- numberPIrecursivo.cpp
+++ numberPIrecursivo.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 float aproxPIrecursivo(int k, int n){
+    //variable auxiliar que me ira acumulando el resultado
     float aprox = 4*((pow(-1,k))/(2*k + 1));
     if(n>0){
-        if(k%2 != 0)
-            return aprox + aproxPIrecursivo(k+1, n-1);
         return aprox + aproxPIrecursivo(k+1, n-1);
     }
     return aprox;
 }
 int main()
 {
-    int k, n;
-    cout<<"ingrese k ";cin>>k;
-    cout<<"ingrese n: ";cin>>n;
-    cout<<"la aproximacion es: "<<aproxPIrecursivo(k,n);
+    //k es desde donde inicia la sumatoria
+    int k = 0;
+    int n;
+    cout<<"ingrese un numero (hasta donde llegara la sumatoria): ";cin>>n;
+    cout<<"la aproximacion es del numero PI es: "<<aproxPIrecursivo(k,n);
     return 0;
 }
